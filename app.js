0943de64d73af44af26c0af16293754b16f39a1a
Sean Swan <ogpswan@YAHOO.COM>
2019-01-13 10:21:35 -0800

    building Pig js game queryselectors and style.display textContent

--- app.js
+++ app.js
@@ -8,3 +8,43 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var score, roundScore, activePlayer, dice;
+
+
+// create the score for each player var scores
+// create variable for Round Score
+// create a variable for the active player 0 = 1st player 1 = 2nd player
+// create dice using math.random() and math.floor() ex math.floor(math.random())
+var scores = [0.0];
+var roundScore = 0;
+activePlayer = 1;
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+// This is a setter setting value
+// using querySelector to grab one element of id then .textContent function to change text in element current-0 which = variable dice
+// switched '#current-0' to '#current-' + activePlayer so when var active player is changed to 1 the current player will be 2 and 0 player is 1
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// this one allows html to be added to dom innerHTML
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; 
+
+//this is a getter getting value
+// prints players score to the console
+var x = document.querySelector('#score-0').textContent;
+console.log(x)
+
+// control css by hiding element
+document.querySelector('.dice').style.display ='none'
+
+
+
+
+
+
+
+
+
+
+
